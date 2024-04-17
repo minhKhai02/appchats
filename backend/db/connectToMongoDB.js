6f0a4351e9baf56264704adc011d472b3fc06edf
minhkhai02 <minhkhai11225@gmail.com>
2024-04-17 11:44:04 +0700

    webchats

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error.message);
+    }
+};
+
+
+export default connectToMongoDB;
